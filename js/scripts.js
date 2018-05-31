224396b1b0ebdb6c8c860d94e3f08962624f6988
Cheshire Cat <thacheshirecat@gmail.com>
2018-05-30 21:44:36 -0700

    Finalize back end/front end logic for addition function, add working subtraction feature.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,36 @@
 //Back End Functions
 
-var add = function(normnum1, normnum2) {
+var add = function(num1, num2) {
 return num1 + num2;
 };
-var subtract = function(normnum1, normnum2) {
-return normnum1 - normnum2;
+var subtract = function(num1, num2) {
+return num1 - num2;
 };
-var multiply = function(normnum1, normnum2) {
-return normnum1 * normnum2;
+var multiply = function(num1, num2) {
+return num1 * num2;
 };
-var divide = function(normnum1, normnum2) {
-return normnum1 / normnum2;
+var divide = function(num1, num2) {
+return num1 / num2;
 };
 
 //Front End Functions
 
 $(document).ready(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
 
-  alert(add(number1, number2));
+$("form#add").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+    $("#addoutput").text(result);
+});
+
+$("form#subtract").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#sub1").val());
+  var number2 = parseInt($("#sub2").val());
+  var result = subtract(number1, number2);
+    $("#suboutput").text(result);
+});
+
 });
